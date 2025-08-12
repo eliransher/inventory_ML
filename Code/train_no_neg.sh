1748db160461f66d9a14057be979331768d4403b
eliransher <scherzter@gmail.com>
2025-08-12 16:35:26 +0300

    initial

--- Code/train_no_neg.sh
+++ Code/train_no_neg.sh
@@ -4,4 +4,4 @@
 #SBATCH --mem 20000
 #SBATCH --gpus-per-node=1
 source /home/eliransc/projects/def-dkrass/eliransc/queues/bin/activate
-python /home/eliransc/projects/def-dkrass/eliransc/inventory_ML/Code/train_no_neg.py
+python /home/eliransc/projects/def-dkrass/eliransc/inventory_ML/Code/train_model.py
